e0a0779c4e21590cecf46e740f7cc9304729d09b
Matspyder51 <mat.spyder@gmail.com>
2015-04-08 19:13:41 +0200

    Add Wire Output and Action to Cap Console

--- lua/entities/cap_console.lua
+++ lua/entities/cap_console.lua
@@ -25,21 +25,42 @@ function ENT:Initialize()
 	self:SetMoveType(MOVETYPE_VPHYSICS);
 	self:SetSolid(SOLID_VPHYSICS);
 	self:SetUseType(SIMPLE_USE);
+
+	self.Pressed = false
+	self:CreateWireOutputs("Active");
 end
 
 function ENT:Think()
-	local ply = StarGate.FindPlayer(self:GetPos(), 400);
+	//local ply = StarGate.FindPlayer(self:GetPos(), 400);
 
-	if (ply and not self.Light) then
+	/*if (ply and not self.Light) then
 		self.Light = true;
 		self:SetSkin(1);
 	elseif (not ply and self.Light) then
 		self.Light = false;
 		self:SetSkin(0);
-	end
+	end*/
 
-	self:NextThink(CurTime()+0.5);
+	//self:NextThink(CurTime()+0.5);
 	return true
+end
+
+function ENT:Use()
+	self:PressConsole();
+end
+
+function ENT:PressConsole()
+	if(not self.Pressed) then
+		self.Pressed = true
+		self:SetSkin(1)
+		self.Light=true
+		self:SetWire("Active",1);
+	else
+		self.Pressed = false
+		self:SetSkin(0)
+		self.Light=true
+		self:SetWire("Active",0);
+	end
 end
 
 end
