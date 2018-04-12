74b07018665263739a51ae0c9f7d02d6863f2b81
TheMerkyShadow <32728280+TheMerkyShadow@users.noreply.github.com>
2018-04-12 04:09:13 -0400

    Update stargate.lua

--- lua/expadv/components/custom/stargate.lua
+++ lua/expadv/components/custom/stargate.lua
@@ -13,7 +13,7 @@ EXPADV.ServerOperators()
 Component:AddPreparedFunction( "stargateTestDial", "e:s", "",
 [[
 if IsValid(@value 1) and @value 1.IsStargate then
-	if EXPADV.PPCheck(Context,@value 1) or Context.player:IsAdmin() then 
+	if EXPADV.PPCheck(Context,@value 1) or @value 1.player:IsAdmin() then 
 		@value 1:DialGate(string.upper(@value 2))
 	end
 end
@@ -32,7 +32,7 @@ Component:AddFunctionHelper( "stargateTestDial", "wl:s", "Dials stargate slowly.
 Component:AddPreparedFunction( "stargateTestDial", "e:s,n", "",
 [[
 if IsValid(@value 1) and @value 1.IsStargate then
-	if EXPADV.PPCheck(Context,@value 1) or Context.player:IsAdmin() then 
+	if EXPADV.PPCheck(Context,@value 1) or @value 1.player:IsAdmin() then 
 		if @value 3 >= 2 then 
 			@value 1:NoxDialGate(string.upper(@value 2)) 
 		else 
@@ -42,11 +42,3 @@ if IsValid(@value 1) and @value 1.IsStargate then
 end
 ]])
 Component:AddFunctionHelper( "stargateTestDial", "e:s,n", "Dials stargate with mode selection." )
-
-Component:AddPreparedFunction( "stargateTestDial", "wl:s,n", "",
-[[
-if IsValid(@value 1) and @value 1.IsStargate then 
-	if @value 3 >= 2 then @value 1:NoxDialGate(string.upper(@value 2)) else @value 1:DialGate(string.upper(@value 2),$util.tobool(@value 3)) end
-end
-]])
-Component:AddFunctionHelper( "stargateTestDial", "wl:s,n", "Dials stargate with mode selection." )
