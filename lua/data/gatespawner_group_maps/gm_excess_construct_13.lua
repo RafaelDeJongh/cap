7e563236cde0623f0eafba1884dd87b962a6fa0a
Matthew-PC <matburren@gmail.com>
2021-04-09 19:09:57 +0100

    Added a bunch of extra gate spawners for popular maps

---
+++ lua/data/gatespawner_group_maps/gm_excess_construct_13.lua
@@ -0,0 +1,190 @@
+[gatespawner]
+version = 3 Group
+
+
+[stargate]
+classname=stargate_atlantis
+position=2413.541016 4106.612305 145.031250
+angles=0.000 -90.058 0.000
+address=SAWNRE
+group=P@
+name=Spawn
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=-1086.741333 -11171.209961 82.031250
+angles=0.000 -90.318 0.000
+address=WATER0
+group=M@
+name=Water
+private=false
+locale=false
+[stargate]
+classname=stargate_universe
+position=11263.932617 -9664.975586 666.031250
+angles=0.000 -89.991 0.000
+address=TANGLE
+group=U@!
+name=Tangle
+private=false
+locale=false
+[stargate]
+classname=stargate_universe
+position=-13839.953125 -4987.922363 172.275406
+angles=-0.000 44.368 0.000
+address=DESRTA
+group=U@!
+name=Desert
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=-5983.814453 -4609.833496 548.031250
+angles=0.000 -0.266 0.000
+address=HIDENR
+group=M@
+name=Hidden room
+private=true
+locale=false
+[stargate]
+classname=stargate_sg1
+position=-3557.888916 2095.368408 762.031250
+angles=0.000 -0.103 0.000
+address=COPLEX
+group=M@
+name=Complex 1
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=-5955.070801 11049.142578 490.031250
+angles=0.000 -1.252 0.000
+address=GAREN1
+group=M@
+name=Garage 1
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=4467.729004 10350.561523 202.031250
+angles=0.000 179.715 0.000
+address=GAREN2
+group=M@
+name=Garage 2
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=-3384.357666 3372.245117 538.531250
+angles=0.000 89.608 0.000
+address=COMLEX
+group=P@
+name=Complex 2
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=10172.758789 1199.981934 90.031250
+angles=0.000 270.636 0.000
+address=GAREN3
+group=P@
+name=Garage 3
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=-7598.346191 -1020.338074 490.031250
+angles=0.000 359.159 0.000
+address=GAREN4
+group=P@
+name=garage 4
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=10066.728516 9216.745117 -9125.968750
+angles=0.000 -0.205 0.000
+address=OASIRE
+group=M@
+name=Oasis
+private=true
+locale=false
+[dhd]
+classname=dhd_atlantis
+position=2157.581543 3879.656738 -14.768750
+angles=15.000 252.102 0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=-1086.917358 -11830.138672 -22.768749
+angles=15.000 -90.072 0.000
+destroyed=false
+[dhd]
+classname=dhd_universe
+position=11458.352539 -9702.620117 561.231262
+angles=15.000 -89.991 -0.000
+destroyed=false
+[dhd]
+classname=dhd_universe
+position=-13670.240234 -4993.522949 17.231255
+angles=15.000 16.379 -0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=-5637.783203 -4488.911133 385.231262
+angles=15.000 33.207 0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=-3239.105957 1906.526123 657.231262
+angles=15.000 -0.512 -0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=-5539.004883 10926.585938 385.231262
+angles=15.000 -3.707 0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=4150.478516 10598.260742 97.231247
+angles=15.000 178.242 -0.000
+destroyed=false
+[dhd]
+classname=dhd_atlantis
+position=9985.429688 1156.426025 -14.768750
+angles=15.000 -132.657 -0.000
+destroyed=false
+[dhd]
+classname=dhd_atlantis
+position=-7570.587891 -1200.378540 385.231262
+angles=15.000 328.060 0.000
+destroyed=false
+[mobile_dhd]
+classname=mobile_dhd
+position=10448.256836 9535.177734 -9154.302734
+angles=0.000 -90.000 0.000
+model=models/props_lab/keypad.mdl
+[sgu_stuff]
+classname=floorchevron
+position=11263.939453 -9704.975586 576.031250
+angles=0.000 90.009 -0.000
+model=models/the_sniper_9/universe/stargate/floorchevron.mdl
+ematerial=The_Sniper_9/Universe/Stargate/UniverseChevronOff.vmt
+[ramp]
+classname=ramp
+position=-13839.953125 -4987.922363 32.275406
+angles=0.000 44.368 -0.000
+model=models/zsdaniel/ramp/ramp.mdl
+[ramp]
+classname=sgc_ramp
+position=-5983.814453 -4609.833496 548.031250
+angles=0.000 -0.266 0.000
+[ramp]
+classname=icarus_ramp
+position=-3383.043945 3564.240723 441.031250
+angles=0.000 89.608 -0.000
+[ramp]
+classname=future_ramp
+position=2413.541016 4106.612305 145.031250
+angles=0.000 -90.058 0.000
---
+++ lua/data/gatespawner_group_maps/gm_functional_flatgrass3.lua
@@ -0,0 +1,120 @@
+[gatespawner]
+version = 3 Group
+
+
+[stargate]
+classname=stargate_universe
+position=-4863.491211 -4655.684082 -11126.968750
+angles=0.000 89.616 0.000
+address=AIRSTP
+group=U@!
+name=Airstrip
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=396.138916 -3037.153076 -14245.968750
+angles=0.000 0.032 0.000
+address=SPAWN0
+group=M@
+name=Spawn
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=-10169.098633 -1537.709961 -15726.968750
+angles=0.000 -0.131 0.000
+address=ROUGHI
+group=P@
+name=Rough hills
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=-9558.142578 -9601.421875 -14187.968750
+angles=-0.000 89.960 0.000
+address=GARES0
+group=M@
+name=Garages
+private=false
+locale=false
+[stargate]
+classname=stargate_atlantis
+position=8520.555664 -415.153534 -14245.968750
+angles=0.000 310.096 0.000
+address=TRAINS
+group=P@
+name=Train stops
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=-15988.754883 4221.790039 -14245.968750
+angles=0.000 297.002 0.000
+address=OVERPA
+group=M@
+name=Overpass
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=2828.514404 6670.962891 -11062.434570
+angles=-0.000 -179.865 0.000
+address=SURFAC
+group=M@
+name=Surface
+private=false
+locale=false
+[dhd]
+classname=dhd_universe
+position=-4726.190918 -4522.898926 -11278.768555
+angles=15.000 44.522 0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=480.353241 -3191.047119 -14350.768555
+angles=15.000 -25.093 0.000
+destroyed=false
+[dhd]
+classname=dhd_atlantis
+position=-10041.182617 -1307.710083 -15886.768555
+angles=15.000 45.208 0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=-9438.306641 -9254.862305 -14350.768555
+angles=15.000 68.273 0.000
+destroyed=false
+[dhd]
+classname=dhd_atlantis
+position=8540.613281 -860.930847 -14350.768555
+angles=15.000 288.572 0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=-16058.300781 3944.540771 -14350.768555
+angles=15.000 283.580 0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=2463.527100 6665.101563 -11222.434570
+angles=15.000 -179.865 -0.000
+destroyed=false
+[ramp]
+classname=ramp
+position=2558.515137 6670.325684 -11233.434570
+angles=-0.000 -179.865 0.000
+model=models/iziraider/ramp4/ramp4.mdl
+[ramp]
+classname=sgu_ramp
+position=-4862.787598 -4550.686523 -11222.968750
+angles=0.000 89.616 -0.000
+model=models/iziraider/sguramp/sgu_ramp.mdl
+[ramp]
+classname=sgc_ramp
+position=-9558.142578 -9601.421875 -14187.968750
+angles=0.000 89.960 -0.000
+[ramp]
+classname=future_ramp
+position=-10169.098633 -1537.709961 -15726.968750
+angles=0.000 -0.131 0.000
