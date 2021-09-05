b2b1260b5a615ada89af95f321c093dfb998e6fb
GameWorld <ehrenf@outlook.com>
2021-09-05 16:31:21 +1000

    added some GateSpawner lua's

---
+++ lua/data/gatespawner_group_maps/gm_construct_13_beta.lua
@@ -0,0 +1,175 @@
+[gatespawner]
+version = 3 Group
+
+
+[stargate]
+classname=stargate_atlantis
+position=1187.316284 6297.323730 108.275398
+angles=0.000 -90.500 0.000
+address=WATERS
+group=M@
+name=Waterside
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=-5187.546875 -1479.059814 -53.968800
+angles=0.000 0.800 -0.000
+address=DARKCH
+group=M@
+name=Dark Room
+private=false
+locale=false
+[stargate]
+classname=stargate_sg1
+position=-2323.649170 -3028.020996 2938.031250
+angles=0.000 90.340 0.000
+address=SKYCRA
+group=M@
+name=Skyscraper
+private=false
+locale=true
+[stargate]
+classname=stargate_tollan_hd
+position=-4178.337891 5340.491699 2254.476318
+angles=-0.025 -179.990 0.004
+address=5ZO4P0
+group=M@
+name=Admin area
+private=false
+locale=true
+[stargate]
+classname=stargate_sg1_hd
+position=-1899.912598 968.977722 -381.580017
+angles=-0.008 -88.837 0.002
+address=0TO2WS
+group=M@
+name=Underground
+private=false
+locale=true
+[stargate]
+classname=stargate_tollan_hd
+position=-2075.965576 -4566.186035 -165.968750
+angles=0.000 89.833 0.000
+address=Q2S9CI
+group=M@
+name=White Room
+private=false
+locale=true
+[stargate]
+classname=stargate_atlantis_hd
+position=2503.974121 5705.085938 -77.968750
+angles=0.000 -90.857 0.000
+address=60J7QX
+group=M@
+name=Under Water Base
+private=false
+locale=true
+[stargate]
+classname=stargate_sg1_hd
+position=473.407806 72.473824 -59.052963
+angles=0.000 357.953 0.000
+address=UX9CFG
+group=M@
+name=Spawn
+private=false
+locale=true
+[dhd]
+classname=dhd_atlantis
+position=1355.333984 6149.300293 -46.768696
+angles=15.000 -52.000 0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=-5064.142578 -1302.962158 -158.768799
+angles=15.000 20.600 -0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1
+position=-2493.044189 -2871.743164 2832.231201
+angles=15.000 89.020 0.000
+destroyed=false
+[dhd]
+classname=dhd_universe
+position=-4230.210449 5187.031738 2113.231201
+angles=15.000 182.197 0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1_hd
+position=-1705.113037 905.611938 -510.331757
+angles=15.000 -89.591 0.000
+destroyed=false
+[dhd]
+classname=dhd_universe
+position=-2225.179443 -4517.650391 -270.768738
+angles=15.000 88.645 0.000
+destroyed=false
+[dhd]
+classname=dhd_atlantis_hd
+position=2660.383789 5652.877930 -182.768753
+angles=15.000 269.779 0.000
+destroyed=false
+[dhd]
+classname=dhd_sg1_hd
+position=520.791504 239.550995 -162.889786
+angles=15.000 -0.991 0.000
+destroyed=false
+[ring_panel]
+classname=ring_panel_goauld
+position=-3232.000000 -1697.268188 98.031303
+angles=0.000 0.580 -0.000
+[ring_panel]
+classname=ring_panel_goauld
+position=908.775330 -1023.502380 -94.419701
+angles=0.000 90.015 -0.294
+[ring_panel]
+classname=ring_panel_goauld
+position=-2063.573486 -2559.780762 -205.968796
+angles=0.000 89.020 0.000
+[ring_panel]
+classname=ring_panel_goauld
+position=-4944.483398 5356.520996 -53.150200
+angles=0.000 179.560 0.000
+[ring_panel]
+classname=ring_panel_goauld
+position=1847.504761 -1600.170288 1181.507324
+angles=-0.000 179.998 -0.653
+[ring_base]
+classname=ring_base_ancient
+position=908.872986 -916.719971 -143.618301
+angles=0.001 94.437 -0.010
+address=1
+[ring_base]
+classname=ring_base_ancient
+position=-3120.313232 -1701.118774 48.031300
+angles=0.000 91.440 -0.000
+address=3
+[ring_base]
+classname=ring_base_ancient
+position=-2065.189941 -2474.619385 15.670500
+angles=0.003 71.669 -179.998
+address=2
+[ring_base]
+classname=ring_base_ancient
+position=-5048.918945 5357.426758 -95.504700
+angles=0.001 -179.992 -0.003
+address=4
+[ring_base]
+classname=ring_base_goauld
+position=1767.558472 -1601.439941 1136.438354
+angles=-0.001 173.831 0.001
+address=5
+[ramp]
+classname=ramp
+position=1187.316284 6297.323730 -31.724600
+angles=0.000 -90.500 0.000
+model=models/zsdaniel/ramp/ramp.mdl
+[ramp]
+classname=ramp
+position=-4358.393066 5340.453125 2128.555420
+angles=-0.025 -179.990 0.004
+model=models/boba_fett/ramps/ramp11.mdl
+[ramp]
+classname=goauld_ramp
+position=-1899.906372 968.957520 -527.580017
+angles=-0.008 -88.837 0.002
