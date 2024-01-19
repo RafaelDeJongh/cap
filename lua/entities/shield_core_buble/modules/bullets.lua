a0035b6a7e02ed01b0e23ea6b958cdde7dc0234e
Gabe <144194708+gabrie155@users.noreply.github.com>
2024-01-18 21:04:50 -0500

    Update bullets.lua

--- lua/entities/shield_core_buble/modules/bullets.lua
+++ lua/entities/shield_core_buble/modules/bullets.lua
@@ -1,9 +1,9 @@
---################# This is the workaround for stopping bullets (FROM SWEPS AND SENTS ONLY!) getting into a shield @aVoN
 hook.Add("StarGate.Bullet", "StarGate.ShieldCore.Bullet", function(self, bullet, trace)
-    local e = trace.Entity;
+    local e = trace.Entity
 
-    if IsValid(e) and (e:GetClass() == "shield_core_buble" or e:GetClass() == "rpg_missile") then
-        -- Call the callback (e.g., to draw effects like bullet tracers!)
+    -- Check if the hit entity is a "shield_core_buble" and is valid
+    if IsValid(e) and e:GetClass() == "shield_core_buble" then
+        -- Call the callback (e.g., to draw effects like bullet tracers)
         if bullet.Callback then
             local dmg = DamageInfo()
             dmg:SetDamage(bullet.Damage or 0)
@@ -12,7 +12,7 @@ hook.Add("StarGate.Bullet", "StarGate.ShieldCore.Bullet", function(self, bullet,
 
         if SERVER then
             -- Draw a bullet tracer into the Shield
-            if bullet.Tracer ~= 0 then
+            if bullet.Tracer and bullet.Tracer ~= 0 then
                 local fx = EffectData()
                 fx:SetStart(bullet.Src)
                 fx:SetOrigin(trace.HitPos)
@@ -21,9 +21,11 @@ hook.Add("StarGate.Bullet", "StarGate.ShieldCore.Bullet", function(self, bullet,
                 util.Effect(bullet.TracerName or "Tracer", fx, true, true)
             end
 
+            -- Ensure that the shield entity handles the bullet hit
             e:Hit(self, trace.HitPos, (bullet.Damage or 20) / 20, -1 * trace.Normal)
         end
 
-        return true -- Tell we override the original bullet!
+        -- Return true to indicate that the custom bullet handling overrides the original behavior
+        return true
     end
 end)
