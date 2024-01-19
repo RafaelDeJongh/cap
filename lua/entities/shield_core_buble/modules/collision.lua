7e9ba6bd6b79d31e145f96bdf6677f025acf4664
Gabe <144194708+gabrie155@users.noreply.github.com>
2024-01-18 21:05:09 -0500

    Update collision.lua

--- lua/entities/shield_core_buble/modules/collision.lua
+++ lua/entities/shield_core_buble/modules/collision.lua
@@ -1,43 +1,52 @@
 function ShieldCoreShouldCollide(ent1, ent2)
 	if ent1 == ent2 then return true end
-	if (!ent1:IsValid() || !ent2:IsValid()) then return true end
+	if not (ent1:IsValid() and ent2:IsValid()) then return true end
 
-	local class  = "shield_core_buble";
-	local world = "worldspawn";
-	local shield;
-	local hitent;
+	local class = "shield_core_buble"
+	local world = "worldspawn"
+	local shield, hitent
 
-	// fast check, if we hitted other shield core (prevent errors)
-	if ((ent1:GetClass() == class) and (ent2:GetClass() == class)) then return false end
+	-- Fast check if we hit another shield core to prevent errors
+	if ent1:GetClass() == class and ent2:GetClass() == class then return false end
 
-	// fast check, if we hitted other prop (here was a bug!)
-	if (ent1:GetClass() == ent2:GetClass()) then return end
+	-- Fast check if we hit the same class (bug fix)
+	if ent1:GetClass() == ent2:GetClass() then return end
 
-	if (ent1:GetClass() == class) then
-		shield = ent1;
-		hitent = ent2;
-	elseif (ent2:GetClass() == class) then
-		shield = ent2;
-		hitent = ent1;
-	else return
+	if ent1:GetClass() == class then
+		shield = ent1
+		hitent = ent2
+	elseif ent2:GetClass() == class then
+		shield = ent2
+		hitent = ent1
+	else
+		return
 	end
 
-	if hitent:IsPlayer() then shield:PlayerPush(hitent) end
+	if hitent:IsPlayer() then
+		shield:PlayerPush(hitent)
+	end
 
-	local hitclass = hitent:GetClass();
-	local shieldown = shield.Parent:GetOwner();
-	local hitown = hitent:GetOwner();
+	local hitclass = hitent:GetClass()
+	local shieldown = shield.Parent:GetOwner()
+	local hitown = hitent:GetOwner()
 
-	// smal check for not colliding with world, shield generator
-	if (hitclass == world or shield.Parent == hitent) then return false end
+	-- Small check for not colliding with world, shield generator
+	if hitclass == world or shield.Parent == hitent then
+		return false
+	end
 
-	 // if enabled and ((hit ent is player or prop owner in table) or (player or prop owner is immunity))
-	if (shield.Enabled and ((table.HasValue(shield.nocollide, hitent) or table.HasValue(shield.nocollide, hitent:GetOwner()))
-	or (shield.Parent.Immunity and (shieldown == hitent or shieldown == hitown)) )) then
-		if hitent:IsPlayer() then shield:PlayerPush(hitent) end // ugly but maybe will work:p
+	-- If enabled and ((hit ent is a player or prop owner in the table) or (player or prop owner is immunity))
+	if shield.Enabled and (
+		(table.HasValue(shield.nocollide, hitent) or table.HasValue(shield.nocollide, hitown)) or
+		(shield.Parent.Immunity and (shieldown == hitent or shieldown == hitown))
+	) then
+		if hitent:IsPlayer() then
+			shield:PlayerPush(hitent)
+		end
 		return false
 	else
 		return
 	end
 end
-hook.Add( "ShouldCollide", "ShieldCoreShouldCollide", ShieldCoreShouldCollide )
+
+hook.Add("ShouldCollide", "ShieldCoreShouldCollide", ShieldCoreShouldCollide)
